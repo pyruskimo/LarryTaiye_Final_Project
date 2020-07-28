ba39024dd7d45b2edfa2428856fc7c5cd1f13226
kj2gh <ben@benlarry.com>
2020-07-28 17:38:20 -0400

    Fix: Updated app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
-from flask import render_template
 
 app = Flask(__name__)
 
@@ -27,8 +26,8 @@ def cities_import() -> List[Dict]:
     return result
 
 
-@app.route('/api/cities')
-def cities() -> str:
+@app.route('/')
+def index() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
