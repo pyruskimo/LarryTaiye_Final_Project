bce798ac225ef02be9bc2ddc93dbd5bc4e6c77bf
kj2gh <ben@benlarry.com>
2020-07-30 00:26:15 -0400

    Fix: Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
 from typing import List, Dict
+import jinja2
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
 from flask import render_template
 
-app = Flask(__name__, template_folder='/Users/bml/dockerfile/Web-Dev-P3/app/template/')
+
+app = Flask(__name__, '/Users/bml/dockerfile/Web-Dev-P3/app/templates')
+
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "index.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+# outputText = template.render()
 
 
 def cities_import() -> List[Dict]:
@@ -32,7 +41,7 @@ def index():
     user = {'username': 'Miguel'}
     cities_data = cities_import()
 
-    return render_template('index.html', title='Home', user=user, cities=cities_data)
+    return render_template("index.html", title='Home', user=user, cities=cities_data)
 
 
 @app.route('/api/cities')
